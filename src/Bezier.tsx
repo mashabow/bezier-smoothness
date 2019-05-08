840da3faf6a4827e8fcea3611bdc82d0b8205739
Masaya Nakamura <mashabow@shiromoji.net>
2019-05-09 00:30:50 +0900

    Draw two handles for each Bezier segment

--- src/Bezier.tsx
+++ src/Bezier.tsx
@@ -2,13 +2,44 @@ import React from 'react';
 
 type Point = [number, number];
 
+// 端点 p から制御点 c へと伸びるハンドル。両点も描画する
+const Handle: React.FC<{p: Point, c: Point}> = ({p, c}) => {
+  const rectSize = 5;
+  return (
+    <g>
+      <line
+        x1={p[0]} y1={p[1]}
+        x2={c[0]} y2={c[1]}
+        stroke="#ccc"
+      />
+      <rect
+        x={p[0] - rectSize/2} y={p[1] - rectSize/2}
+        width={rectSize} height={rectSize}
+        fill="white"
+        stroke="black"
+      />
+      <circle
+        cx={c[0]} cy={c[1]}
+        r={3}
+        fill="white"
+        stroke="#999"
+      />
+    </g>
+  );
+};
+
+// 始点 p0, 制御点 c0, c1, 終点 p1 によって定義される 3 次ベジエ曲線
 const Bezier: React.FC<{p0: Point, c0: Point, c1: Point, p1: Point}> = ({p0, c0, c1, p1}) => {
   return (
-    <path
-      d={`M ${p0} C ${c0} ${c1} ${p1}`}
-      fill="none"
-      stroke="black"
-    />
+    <g>
+      <path
+        d={`M ${p0} C ${c0} ${c1} ${p1}`}
+        fill="none"
+        stroke="black"
+      />
+      <Handle p={p0} c={c0}/>
+      <Handle p={p1} c={c1}/>
+    </g>
   );
 }
 
