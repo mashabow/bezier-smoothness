0bf149108b4e6ab1c1955b7040c5e9c1e61f94ef
Masaya Nakamura <mashabow@shiromoji.net>
2019-05-10 23:43:50 +0900

    Improve the app's main style
    https://material-ui.com/style/css-baseline/
    https://material-ui.com/css-in-js/basics/#hook-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import {makeStyles} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React, {useContext, useCallback} from 'react';
 
 import {Store, Provider} from './store';
 import Bezier from './Bezier';
 import TSlider from './TSlider';
-import styles from './App.module.scss';
+
+const svgSize = 500;
+
+const useStyles = makeStyles({
+  root: {
+    margin: 'auto',
+    display: 'flex',
+    justifyContent: 'center',
+    // 上下中央に
+    alignItems: 'center',
+    height: '100vh',
+  },
+  svg: {
+    flex: 'none',
+    backgroundColor: 'white',
+  },
+  controls: {
+    width: 300,
+    height: svgSize,
+    padding: 20,
+    // スライダ操作するとスクロールバーが表示されてしまうようなので、それを回避
+    overflow: 'hidden',
+  },
+});
 
 const App: React.FC = () => {
+  const classes = useStyles();
   const {state, dispatch} = useContext(Store);
   const dispatchDrag = useCallback(
     (e: React.MouseEvent) => dispatch({
@@ -21,17 +47,20 @@ const App: React.FC = () => {
 
   return (
     <div
-      className={styles.App}
+      className={classes.root}
       onMouseMove={state.draggingPoint ? dispatchDrag : undefined}
       onMouseUp={state.draggingPoint ? dispatchDragEnd : undefined}
     >
       <svg
-        width="500"
-        height="500"
+        className={classes.svg}
+        width={svgSize}
+        height={svgSize}
       >
         <Bezier {...state.bezier} />
       </svg>
-      <TSlider />
+      <div className={classes.controls}>
+        <TSlider />
+      </div>
     </div>
   );
 }
@@ -39,6 +68,7 @@ const App: React.FC = () => {
 // Provider は、useContext を使う FC より外側にある必要がある
 export default () => (
   <Provider>
+    <CssBaseline />
     <App />
   </Provider>
 );
