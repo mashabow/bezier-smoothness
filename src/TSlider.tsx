643f6706a3fa6a0f4a0bdeee5de84246ba3f5857
Masaya Nakamura <mashabow@shiromoji.net>
2019-05-11 19:11:58 +0900

    Adjust TSlider's style

--- src/TSlider.tsx
+++ src/TSlider.tsx
@@ -1,10 +1,24 @@
+import {makeStyles} from '@material-ui/styles';
 import Slider from '@material-ui/lab/Slider';
 import Typography from '@material-ui/core/Typography';
 import React, {useContext, useCallback} from 'react';
 
 import {Store} from './store';
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  label: {
+    flex: 'none',
+    paddingLeft: 10,
+  },
+});
+
 const TSlider: React.FC = () => {
+  const classes = useStyles();
   const {state, dispatch} = useContext(Store);
   const dispatchSetT = useCallback(
     (_, value: number) => dispatch({type: 'SET_T', payload: value}),
@@ -12,7 +26,7 @@ const TSlider: React.FC = () => {
   );
 
   return (
-    <div>
+    <div className={classes.root}>
       <Slider
         value={state.bezier.t}
         min={0}
@@ -20,7 +34,10 @@ const TSlider: React.FC = () => {
         step={0.01}
         onChange={dispatchSetT}
       />
-      <Typography variant="body1">
+      <Typography
+        variant="body1"
+        className={classes.label}
+      >
         t = {state.bezier.t.toFixed(2)}
       </Typography>
     </div>
